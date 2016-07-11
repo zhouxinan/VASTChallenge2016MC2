36f8436230d694f06a90de0ee12a395d9d3be4e4
Zhou Xinan <zhouxinan@protonmail.ch>
2016-07-11 10:11:56 -0400

    Implement a naive version of parsing JSON.

--- KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/App.java
+++ KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/App.java
@@ -1,13 +1,35 @@
 package com.zhouxinan.KLDAnalysis;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class App {
+	public static void main(String[] args) {
+		JSONParser parser = new JSONParser();
+		try {
+			Object object = parser.parse(new FileReader("json/proxOut-MC2.json"));
+			JSONArray array = (JSONArray) object;
+			for (Object o : array) {
+				JSONObject record = (JSONObject) o;
+				Double offset = (Double) record.get("offset");
+				System.out.println(offset);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
