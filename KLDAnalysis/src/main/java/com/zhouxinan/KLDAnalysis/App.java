4dd307513730aca68a1e635917a8c94580a93424
Zhou Xinan <zhouxinan@protonmail.ch>
2016-07-11 09:16:48 -0400

    Create project.

---
+++ KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/App.java
@@ -0,0 +1,13 @@
+package com.zhouxinan.KLDAnalysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
