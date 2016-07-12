a92ad84f9f7ba990fd70e9ce1ded57827813c167
Zhou Xinan <zhouxinan@protonmail.ch>
2016-07-12 15:09:34 -0400

    Implement calculation of probability of a person's daily occurrence.

--- KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/Analysis.java
+++ KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/Analysis.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Analysis {
+	Dao dao = Dao.getInstance();
+
 	public List<String> getAllProxCard() throws SQLException {
 		Dao dao = Dao.getInstance();
 		return dao.selectAllProxCard();
@@ -14,22 +16,53 @@ public class Analysis {
 		Dao dao = Dao.getInstance();
 		return dao.selectAllProxSensorDataOfProxCard(proxCard);
 	}
-	
+
 	public List<String> getDistinctDateOfProxCard(String proxCard) throws SQLException {
 		Dao dao = Dao.getInstance();
 		return dao.selectDistinctDateOfProxCard(proxCard);
 	}
 
+	public List<ProxSensorData> getProxSensorDataListByDateAndProxCard(String proxCard, String date)
+			throws SQLException {
+		Dao dao = Dao.getInstance();
+		return dao.selectByDateAndProxCard(proxCard, date);
+	}
+
+	public void insertToAnalysisTable(String proxCard, String zone, String date, Integer floor, Double duration)
+			throws SQLException {
+		Dao dao = Dao.getInstance();
+		dao.insertToAnalysisTable(proxCard, zone, date, floor, duration);
+	}
+
 	public void analyze() throws SQLException {
 		List<String> proxCardList = getAllProxCard();
-//		for (Iterator<String> iterator = proxCardList.iterator(); iterator.hasNext();) {
-//			String proxCard = (String) iterator.next();
-			String proxCard = "vawelon";
+		for (Iterator<String> iterator = proxCardList.iterator(); iterator.hasNext();) {
+			String proxCard = (String) iterator.next();
+			// String proxCard = "vawelon";
 			List<String> dateList = getDistinctDateOfProxCard(proxCard);
 			for (Iterator<String> iterator2 = dateList.iterator(); iterator2.hasNext();) {
-				System.out.println((String) iterator2.next());
+				String date = (String) iterator2.next();
+				List<ProxSensorData> psdList = getProxSensorDataListByDateAndProxCard(proxCard, date);
+				System.out.println("length: " + psdList.size());
+				Iterator<ProxSensorData> iterator3 = psdList.iterator();
+				ProxSensorData proxSensorData = (ProxSensorData) iterator3.next();
+				Double offset = proxSensorData.getOffset();
+				Double firstOffset = offset;
+				Integer floor = proxSensorData.getFloor();
+				String zone = proxSensorData.getZone();
+				while (iterator3.hasNext()) {
+					ProxSensorData currentPsd = (ProxSensorData) iterator3.next();
+					Double duration = currentPsd.getOffset() - offset;
+					insertToAnalysisTable(proxCard, zone, date, floor, duration);
+					offset = currentPsd.getOffset();
+					floor = currentPsd.getFloor();
+					zone = currentPsd.getZone();
+				}
+				Double durationOfDay = offset - firstOffset;
+				dao.calculateProbabilityForDay(date, durationOfDay);
+
 			}
-//		}
+		}
 	}
 
 }
--- KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/Dao.java
+++ KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/Dao.java
@@ -123,7 +123,7 @@ public class Dao {
 		}
 		return null;
 	}
-	
+
 	public List<String> selectDistinctDateOfProxCard(String proxCard) throws SQLException {
 		Connection con = null;
 		Statement sm = null;
@@ -131,7 +131,8 @@ public class Dao {
 		try {
 			con = DriverManager.getConnection(url, dbUsername, dbPassword);
 			sm = con.createStatement();
-			String sql = "select distinct date(datetime) as date from prox_sensor_data where proxCard = '" + proxCard + "';";
+			String sql = "select distinct date(datetime) as date from prox_sensor_data where proxCard = '" + proxCard
+					+ "';";
 			results = sm.executeQuery(sql);
 			List<String> dateList = new LinkedList<String>();
 			while (results.next()) {
@@ -150,5 +151,84 @@ public class Dao {
 			}
 		}
 		return null;
+	}
+
+	public List<ProxSensorData> selectByDateAndProxCard(String proxCard, String date) throws SQLException {
+		Connection con = null;
+		Statement sm = null;
+		ResultSet results = null;
+		try {
+			con = DriverManager.getConnection(url, dbUsername, dbPassword);
+			sm = con.createStatement();
+			String sql = "SELECT * FROM prox_sensor_data WHERE datetime BETWEEN '" + date + " 00:00:00' AND '" + date
+					+ " 23:59:59' and proxCard = '" + proxCard + "';";
+			results = sm.executeQuery(sql);
+			List<ProxSensorData> psdList = new LinkedList<ProxSensorData>();
+			while (results.next()) {
+				ProxSensorData psd = new ProxSensorData();
+				psd.setFloor(results.getInt("floor"));
+				psd.setZone(results.getString("zone"));
+				psd.setOffset(results.getDouble("offset"));
+				psdList.add(psd);
+			}
+			return psdList;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (sm != null) {
+				sm.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
+		return null;
+	}
+
+	public void insertToAnalysisTable(String proxCard, String zone, String date, Integer floor, Double duration)
+			throws SQLException {
+		Connection con = null;
+		Statement sm = null;
+		try {
+			con = DriverManager.getConnection(url, dbUsername, dbPassword);
+			sm = con.createStatement();
+			String sql = "INSERT INTO daily_data (proxCard,datetime,floor,zone,duration) VALUES ('" + proxCard + "', '"
+					+ date + " 00:00:00', '" + floor + "', '" + zone + "', '" + duration
+					+ "') ON DUPLICATE KEY UPDATE duration=duration+" + duration + ";";
+			sm.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (sm != null) {
+				sm.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	public void calculateProbabilityForDay(String date, Double durationOfDay) throws SQLException {
+		Connection con = null;
+		Statement sm = null;
+		try {
+			con = DriverManager.getConnection(url, dbUsername, dbPassword);
+			sm = con.createStatement();
+			String sql = "UPDATE daily_data SET probability=duration/" + durationOfDay + " WHERE `datetime` between '"
+					+ date + " 00:00:00' and '" + date + " 23:59:59';";
+			sm.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (sm != null) {
+				sm.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
 	}
 }
