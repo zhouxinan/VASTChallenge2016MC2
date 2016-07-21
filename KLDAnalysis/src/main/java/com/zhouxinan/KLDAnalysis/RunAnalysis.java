233b101237339955feb90f78537ddc0f79d38eeb
Zhou Xinan <zhouxinan@protonmail.ch>
2016-07-21 14:30:44 -0400

    Run for symmetrical result.

--- KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/RunAnalysis.java
+++ KLDAnalysis/src/main/java/com/zhouxinan/KLDAnalysis/RunAnalysis.java
@@ -7,18 +7,17 @@ public class RunAnalysis {
 	public static void main(String[] args) throws SQLException, IOException {
 		Analysis analysis = new Analysis();
 //		analysis.buildDailyDataTable();
-//		analysis.calculateKLDPerPerson(true);
-//		analysis.calculateKLDPerPerson(false);
-//		analysis.calculateKLDOfSortedHistogramPerPerson();
+		analysis.calculateKLDPerPerson(true, true);
+		analysis.calculateKLDPerPerson(false, true);
+		analysis.calculateKLDOfSortedHistogramPerPerson(true);
 //		analysis.calculateAverageKLDPerDayPerPerson(true);
 //		analysis.calculateAverageKLDPerDayPerPerson(false);
 //		analysis.calculateAverageKLDOfSortedHistogramPerDayPerPerson();
-//		analysis.calculateKLDPerDepartment(true);
-//		analysis.calculateKLDPerDepartment(false);
-//		analysis.calculateKLDOfSortedHistogramPerDepartment();
+		analysis.calculateKLDPerDepartment(true, true);
+		analysis.calculateKLDPerDepartment(false, true);
+		analysis.calculateKLDOfSortedHistogramPerDepartment(true);
 //		analysis.calculateAverageKLDPerDepartment(true);
 //		analysis.calculateAverageKLDPerDepartment(false);
 //		analysis.calculateAverageKLDOfSortedHistogramPerDepartment();
-		analysis.calculateKLDPerPerson(true, false);
 	}
 }
